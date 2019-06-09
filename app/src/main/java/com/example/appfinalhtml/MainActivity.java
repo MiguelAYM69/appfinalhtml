97addc175e93d08fedd0f87aac72114fa08930e0
MiguelAYM69 <48419295+MiguelAYM69@users.noreply.github.com>
2019-06-09 14:06:48 -0500

    sonidos animales miguel

--- app/src/main/java/com/example/appfinalhtml/MainActivity.java
+++ app/src/main/java/com/example/appfinalhtml/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appfinalhtml;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -9,14 +10,82 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mja,mgu,mco,mte,mga,mpu,mjab,mlo;
+    ImageButton jaguar,guacamaya,colibri,tejon,gato,puerquito,jabali,loro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jaguar= (ImageButton) findViewById(R.id.jaguar);
+        mja=MediaPlayer.create(this, R.raw.jaguar);
+        jaguar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mja.start();
+            }
+        });
+        guacamaya= (ImageButton) findViewById(R.id.guacamaya);
+        mgu=MediaPlayer.create(this, R.raw.guacamaya);
+        guacamaya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mgu.start();
+            }
+        });
+        colibri= (ImageButton) findViewById(R.id.colibri);
+        mco=MediaPlayer.create(this, R.raw.colibri);
+        colibri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mco.start();
+            }
+        });
+        tejon= (ImageButton) findViewById(R.id.tejon);
+        mte=MediaPlayer.create(this, R.raw.tejon);
+        tejon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mte.start();
+            }
+        });
+        gato= (ImageButton) findViewById(R.id.gato);
+        mga=MediaPlayer.create(this, R.raw.gato);
+        gato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mga.start();
+            }
+        });
+        puerquito= (ImageButton) findViewById(R.id.puerquito);
+        mpu=MediaPlayer.create(this, R.raw.puerquito);
+        puerquito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpu.start();
+            }
+        });
+        jabali= (ImageButton) findViewById(R.id.jabali);
+        mjab=MediaPlayer.create(this, R.raw.jabali);
+        jabali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mjab.start();
+            }
+        });
+       loro= (ImageButton) findViewById(R.id.loro);
+        mlo=MediaPlayer.create(this, R.raw.loro);
+        loro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mlo.start();
+            }
+        });
     }
+
 
     public void ColoresIntent(View view) {
         Intent a = new Intent(MainActivity.this,Colores.class);
@@ -50,6 +119,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
