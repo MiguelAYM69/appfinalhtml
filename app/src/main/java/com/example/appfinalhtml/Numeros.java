715196dd1a2f38428cea306e6062002ffd4c03b7
MARIA <majoibarra1022@gmail.com>
2019-06-10 23:06:25 -0500

    imagebutton ya declarados en numeros, faltan los audios

--- app/src/main/java/com/example/appfinalhtml/Numeros.java
+++ app/src/main/java/com/example/appfinalhtml/Numeros.java
@@ -3,15 +3,23 @@ package com.example.appfinalhtml;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Numeros extends AppCompatActivity {
 
+
+    ImageButton uno, dos, tres, cuatro, cinco, seis, siete, ocho,nueve,diez,once, doce, trece, catorce, quince, deiciseis, diescisiete, diesciocho, dieciocho, diecinueve;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numeros);
+
+
+
+
 
     }
 
